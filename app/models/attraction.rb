b7e02a046574644e59718d8f33fc0bd2731ba42d
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-19 02:44:00 +0000

    Automatically backed up by Learn

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,5 @@
 class Attraction < ApplicationRecord
+  validates_presence_of :name, :min_height, :nausea_rating, :happiness_rating, :ticket
+  has_many :rides
+  has_many :users, through: :rides
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,51 @@
 class User < ActiveRecord::Base
+  validates_presence_of :name, :password, :happiness, :nausea, :height, :tickets
+  has_many :rides
+  has_many :attractions, through: :rides
+  admin: :defaults => false
+  
+  def mood(nausea, happiness)
+    nausea > happiness ? "sad" : "happy"
+  end
 end
+
+
+
+it "is valid with a name, password, happiness, nausea, height, and tickets" do
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a password" do
+    expect(User.new(name: "Name")).not_to be_valid
+  end
+
+  it "is valid with an admin boolean" do
+    expect(admin).to be_valid
+  end
+
+  it "defaults to admin => false" do
+    expect(user.admin).to eq(false)
+  end
+
+  it "has many rides" do
+    first_ride = Ride.create(:user_id => user.id, :attraction_id => roller_coaster.id)
+    second_ride = Ride.create(:user_id => user.id, :attraction_id => ferris_wheel.id)
+    expect(user.rides.first).to eq(first_ride)
+    expect(user.rides.last).to eq(second_ride)
+  end
+
+  it "has many attractions through rides" do
+    user.attractions << [roller_coaster, ferris_wheel]
+    expect(user.attractions.first).to eq(roller_coaster)
+    expect(user.attractions.last).to eq(ferris_wheel)
+  end
+
+  it "has a method 'mood' that returns 'sad' when the user is more nauseous than happy" do
+    expect(user.mood).to eq("sad")
+  end
+
+  it "has a method 'mood' that returns 'happy' when the user is more happy than nauseous" do
+    user.update(:happiness => 7)
+    expect(user.mood).to eq("happy")
+  end
+
